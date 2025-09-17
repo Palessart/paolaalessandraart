3d9163eaa980a543ad6e1dc8e861f91a8d62ab17
Palessart <paolaalessandraart@gmail.com>
2025-09-16 21:16:09 -0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,9 @@
 setTimeout(function() {backgroundImage(); }, 5000); 
 
 function backgroundImage(){
-  document.getElementById("mainBackgroundImage").mainstyle.backgroundImage="url('/design/images/background.png')"; 
+  document.getElementById("mainBackgroundImage").style.color= red; 
 }
+
 
 
 
