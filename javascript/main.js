3021ad029b6eddd2b71c5da7bd9fca391cc8b4a9
Palessart <paolaalessandraart@gmail.com>
2025-10-19 12:24:35 -0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,21 @@
-setTimeout(function() {backgroundImage(); }, 5000); 
+setTimeout(function() {mainDivWrapper(); }, 5000); 
 
-function backgroundImage(){
-  let image = document.body.style.backgroundImage;
-  fadeIn(image);
+function mainDivWrapper(){
+  let mainDiv = document.getElementById(wrapper);
+  fadeIn(mainDiv);
 }
 
-function fadeIn(image) {
-    image.style.opacity = 0;
+function fadeIn(mainDiv) {
+    mainDiv.style.opacity = 0;
     var showingImage = function () {
-        image.style.opacity = +image.style.opacity + 0.01;
-        if (+image.style.opacity < 1) {
+        mainDiv.style.opacity = +mainDiv.style.opacity + 0.01;
+        if (+mainDiv.style.opacity < 1) {
             (window.requestAnimationFrame && requestAnimationFrame(showingImage)) || setTimeout(showingImage, 16)
         }
     };
     showingImage();
 }
+
 
 
 
