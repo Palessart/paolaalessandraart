358ade45c0b130f1ae8c690c00cf8c3c145facac
Palessart <paolaalessandraart@gmail.com>
2025-10-27 12:40:26 -0600

    Update backgroundImage.js

--- javascript/backgroundImage.js
+++ javascript/backgroundImage.js
@@ -9,8 +9,8 @@ function backgroundImage(){
 function fadeIn(image) {
     const example = document.getElementById('bodyID').style.backgroundImage;
     image.style.opacity = 0;
-    const example = document.getElementById('bodyID').style.backgroundImage;
     document.getElementById('bodyID').style.backgroundImage = image;
+    const example2 = document.getElementById('bodyID').style.backgroundImage;
     var showingImage = function () {
         image.style.opacity = +image.style.opacity + 0.01;
         if (+image.style.opacity < 1) {
