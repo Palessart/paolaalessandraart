e43d7274151b54ebd9ebf9d886065890c768fce2
Palessart <paolaalessandraart@gmail.com>
2025-10-30 14:24:14 -0600

    Update backgroundImage.js

--- javascript/backgroundImage.js
+++ javascript/backgroundImage.js
@@ -1,18 +1,1 @@
-setTimeout(function() {backgroundImage(); }, 9000); 
 
-function backgroundImage(){
-  var bodyID = document.getElementById("bodyID"); 
-  fadeIn(bodyID);
-}
-
-function fadeIn(bodyID) {
-    bodyID.style.setProperty('--backgroundURL', "url('https://paolaalessandraart.click/design/images/background.png')");
-    bodyID.style.setProperty('opacity', 0);
-    var showingImage = function () {
-        bodyID.style.opacity = +bodyID.style.opacity + 0.01; 
-        if (+bodyID.style.opacity < 1) {
-            (window.requestAnimationFrame && requestAnimationFrame(showingImage)) || setTimeout(showingImage, 16)
-        }
-    };
-    showingImage();
-}
