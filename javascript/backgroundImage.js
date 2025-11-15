02a12fdfff258e657a30e16ecf01ea1c68b1fb64
Palessart <paolaalessandraart@gmail.com>
2025-11-15 12:06:51 -0600

    Update backgroundImage.js

--- javascript/backgroundImage.js
+++ javascript/backgroundImage.js
@@ -1,1 +1,17 @@
+setTimeout(function() {bodyBackgroundDIV(); }, 2000); 
 
+function bodyBackgroundDIV(){
+  var bodyDiv = document.getElementById("bodyDIVContainer");
+  fadeIn(bodyDiv);
+}
+
+function fadeIn(bodyDiv) {
+    bodyDiv.style.opacity = 0;
+    var showingImage = function () {
+        bodyDiv.style.opacity = +bodyDiv.style.opacity + 0.01;
+        if (+bodyDiv.style.opacity < 1) {
+            (window.requestAnimationFrame && requestAnimationFrame(showingImage)) || setTimeout(showingImage, 16)
+        }
+    };
+    showingImage();
+}
