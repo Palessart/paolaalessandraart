e56af0c3b781683c6c21233ffdfcc83d0139ca03
Palessart <paolaalessandraart@gmail.com>
2025-11-15 19:48:18 -0600

    Update backgroundImage.js

--- javascript/backgroundImage.js
+++ javascript/backgroundImage.js
@@ -8,8 +8,8 @@ function bodyBackgroundDIV(){
 function fadeIn(bodyDiv) {
     bodyDiv.style.opacity = 1;
     var showingImage = function () {
-        bodyDiv.style.opacity = +bodyDiv.style.opacity - 0.01;
-        if (+bodyDiv.style.opacity > 0) {
+        bodyDiv.style.opacity = -bodyDiv.style.opacity - 0.01;
+        if (-bodyDiv.style.opacity > 0) {
             (window.requestAnimationFrame && requestAnimationFrame(showingImage)) || setTimeout(showingImage, 16)
         }
     };
